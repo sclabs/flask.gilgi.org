04a0208dec56991325132a3deb6e82d56c613a32
Thomas Gilgenast <thomasgilgenast@gmail.com>
2012-09-23 04:04:22 -0400

    real telnet code

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 import os
-import PyTS3
+#import PyTS3
+import telnetlib
 from flask import Flask, send_from_directory
 from timeout import timeout
 
 app = Flask(__name__)
 
-@timeout(2, False)
+"""
 def isOnline(address, port=10011):
     server = PyTS3.ServerQuery(address, port)
     try:
         status = server.connect()
         return status
+    except:
+        return False
+"""
+
+def telnet(address, port=10011, timeout=2):
+    try:
+        connection = telnetlib.Telnet(address, port, timeout)
+        output = connection.read_until('TS3', timeout)
+        if not output.endswith('TS3'):
+            return False
+        return True
     except:
         return False
 
@@ -20,13 +32,15 @@ def hello():
 
 @app.route('/tsstatus', methods=['GET'])
 def tsstatus():
-    if isOnline('ts.gilgi.org'):
+    #if isOnline('ts.gilgi.org'):
+    if telnet('ts.gilgi.org'):
         return send_from_directory("static", "tsonline.js", mimetype="text/javascript")
     return send_from_directory("static", "tsoffline.js", mimetype="text/javascript")
 
 @app.route('/awsstatus', methods=['GET'])
 def awsstatus():
-    if isOnline('aws.gilgi.org'):
+    #if isOnline('aws.gilgi.org'):
+    if telnet('aws.gilgi.org'):
         return send_from_directory("static", "awsonline.js", mimetype="text/javascript")
     return send_from_directory("static", "awsoffline.js", mimetype="text/javascript")
 
