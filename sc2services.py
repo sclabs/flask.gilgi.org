e608c25b458921f8eed99333a7da6d7dcd8b9d9e
Thomas Gilgenast <thomasgilgenast@gmail.com>
2012-10-27 09:08:29 -0400

    added levels to data for sc2

--- sc2services.py
+++ sc2services.py
@@ -28,12 +28,21 @@ def getdata():
         player['url'] = sc2ranks.character_url(character['region'],
                                             character['name'],
                                             character['id'])
+        player['levels'] = [0, 0, 0, 0]
 
         # fill in hard fields with real values
         for team in info.teams:
             if team.bracket == 1 or team.is_random:
                 player['leagues'][team.bracket - 1] = team.league
                 player['ranks'][team.bracket - 1] = team.division_rank
+                if team.division_rank <= 8:
+                    player['levels'][team.bracket - 1] = 4;
+                else if team.division_rank <= 25:
+                    player['levels'][team.bracket - 1] = 3;
+                else if team.division_rank <= 50:
+                    player['levels'][team.bracket - 1] = 2;
+                else:
+                    player['levels'][team.bracket - 1] = 1;
 
         # append this player to the list
         data['players'].append(player)
