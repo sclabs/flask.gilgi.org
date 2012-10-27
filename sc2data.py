f617df64289a927520fd01d759c40202a70a94ab
Thomas Gilgenast <thomasgilgenast@gmail.com>
2012-10-27 07:20:23 -0400

    removed old code

--- sc2data.py
+++ sc2data.py
@@ -6,6 +6,3 @@ characters = ({'region': "us",
               {'region': "us",
                'name': "Gilgi",
                'id': "1091232"},)
-
-"""characters = (("us", "Deathenator", "2259599"),
-              ("us", "Gilgi",       "1091232"))"""
