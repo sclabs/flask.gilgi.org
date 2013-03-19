2a87aadd4f98c45ffd6e30b41333cdebf15e2faa
Thomas Gilgenast <thomasgilgenast@gmail.com>
2013-03-19 13:47:36 -0500

    bugfix part 2

--- dota2services.py
+++ dota2services.py
@@ -13,9 +13,21 @@ def getdata():
 
     # loop through the sc teams
     for teamid in dota2data.teamids:
+        # get the team info
         team = teamobject.teams(start_at_team_id=teamid,teams_requested=1)[0]
-        team.url = 'http://dotabuff.com/teams/' + str(team.team_id)
-        data['teams'].append(team)
+
+        # this dict will store the team's information
+        teaminfo = {}
+
+        # put the info we want in to the dict
+        teaminfo['name'] = team.name
+        teaminfo['tag'] = team.tag
+        teaminfo['games_played'] = team.games_played_with_current_roster
+        teaminfo['rating'] = team.rating
+        teaminfo['url'] = 'http://dotabuff.com/teams/' + str(team.team_id)
+
+        # add this team to the list
+        data['teams'].append(teaminfo)
 
     # return the data
     return data
